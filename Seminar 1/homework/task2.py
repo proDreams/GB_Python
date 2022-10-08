563d2ca0776da99dc3b389ec317044f14321035e
proDream <sushkoos@gmail.com>
2022-10-08 14:07:50 +0400

    small fix hw task2

--- Seminar 1/homework/task2.py
+++ Seminar 1/homework/task2.py
@@ -4,7 +4,7 @@ result_list = []
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            result_list.append(not(x or y or z) == (not x) and (not y) and (not z))
+            result_list.append(not (x or y or z) == (not x and not y and not z))
 for i in result_list:
     if result_list[0] != i:
         print("Утверждение не верно")
