2a94a0adbfffbcfa22d4f01c2a115f571cc7d8ab
proDream <sushkoos@gmail.com>
2022-10-08 15:29:45 +0400

    update hw task2

--- Seminar 1/homework/task2.py
+++ Seminar 1/homework/task2.py
@@ -1,15 +1,21 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 #
-result_list = []
+
+# Первый вариант - традиционный
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            print(f'x = {x}, y = {y}, z = {z} = Утверждение верно' if not (x or y or z) == (not x and not y and not z) else 'Утверждение не верно')
-
+            print(f'x = {x}, y = {y}, z = {z} = Утверждение верно' if not (x or y or z) == (
+                        not x and not y and not z) else 'Утверждение не верно')
 
 # print('Утверждение верно' if not (0 or 0 or 0) == (not 0 and not 0 and not 0) else 'Утверждение не верно')
 
-# Второй очень страшный и ни разу не изящный в одну строку
+# Второй вариант - очень страшный и ни разу не изящный в одну строку
 print(*[f'x = {x}, y = {y}, z = {z} = Утверждение верно' if not (x or y or z) == (
-            not x and not y and not z) else 'Утверждение не верно' for z in range(2) for y in range(2) for x in
+        not x and not y and not z) else 'Утверждение не верно' for z in range(2) for y in range(2) for x in
         range(2)], sep='\n')
+
+# Третий вариант - почище, с использованием all()
+print('Утверждение верно для всех предикат' if all(
+    [not (x or y or z) == (not x and not y and not z) for z in range(2) for y in range(2) for x in
+     range(2)]) else 'Утверждение не верно')
