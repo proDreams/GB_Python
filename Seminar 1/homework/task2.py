9b62201d8f091e6ea4a822fbc626c442cb15fc0b
proDream <sushkoos@gmail.com>
2022-10-08 16:13:09 +0400

    update hw task2

--- Seminar 1/homework/task2.py
+++ Seminar 1/homework/task2.py
@@ -6,7 +6,7 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             print(f'x = {x}, y = {y}, z = {z} = Утверждение верно' if not (x or y or z) == (
-                        not x and not y and not z) else 'Утверждение не верно')
+                    not x and not y and not z) else 'Утверждение не верно')
 
 # print('Утверждение верно' if not (0 or 0 or 0) == (not 0 and not 0 and not 0) else 'Утверждение не верно')
 
@@ -18,4 +18,4 @@ print(*[f'x = {x}, y = {y}, z = {z} = Утверждение верно' if not 
 # Третий вариант - почище, с использованием all()
 print('Утверждение верно для всех предикат' if all(
     not (x or y or z) == (not x and not y and not z) for z in range(2) for y in range(2) for x in
-     range(2)) else 'Утверждение не верно')
+    range(2)) else 'Утверждение не верно')
