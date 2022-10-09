2e3f6b3076a527c07e573ef51466f325de4a3e89
proDream <sushkoos@gmail.com>
2022-10-09 13:21:18 +0400

    update

--- Seminar 1/homework/task2.py
+++ Seminar 1/homework/task2.py
@@ -2,11 +2,17 @@
 #
 
 # Первый вариант - традиционный
+result_list = []
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            print(f'x = {x}, y = {y}, z = {z} = Утверждение верно' if not (x or y or z) == (
-                    not x and not y and not z) else 'Утверждение не верно')
+            result_list.append(not (x or y or z) == (not x and not y and not z))
+for i in result_list:
+    if result_list[0] != i:
+        print("Утверждение не верно")
+        break
+else:
+    print("Утверждение верно")
 
 # print('Утверждение верно' if not (0 or 0 or 0) == (not 0 and not 0 and not 0) else 'Утверждение не верно')
 
