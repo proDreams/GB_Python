325dc2a5e726198576fb97959a79cfb08aced200
proDream <sushkoos@gmail.com>
2022-10-12 00:17:01 +0400

    Update seminar 2 hw

--- Seminar 2/homework/task4.py
+++ Seminar 2/homework/task4.py
@@ -2,6 +2,7 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
+# -n, n
 num = int(input("Введите число: "))
 num_list = [i for i in range(-num, num + 1)]
 indexes = open('task4.txt', 'r')
@@ -12,3 +13,16 @@ for i in indexes:
     prod_num *= num_list[int(i)]
 print(num_list, prod_num, sep='\n')
 
+
+# random
+from random import randint
+
+num = int(input("Введите число: "))
+num_list = [randint(-num, num) for i in range(num)]
+indexes = open('task4.txt', 'r')
+prod_num = 1
+for i in indexes:
+    if int(i) > num:
+        continue
+    prod_num *= num_list[int(i)]
+print(num_list, prod_num, sep='\n')
