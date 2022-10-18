d27b21d026581e402a3f837a1b8c96e4a52fe53a
proDream <sushkoos@gmail.com>
2022-10-18 16:14:35 +0400

    Add partitial seminar 4 Homework

---
+++ Seminar 4/homework/task2.py
@@ -0,0 +1,26 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+def check_prime(number, lst):
+    if number in lst:
+        return True
+    for j in range(2, number):
+        if number % j == 0:
+            return False
+    else:
+        if number not in lst:
+            lst.append(number)
+        return True
+
+
+numb = int(input("Введите число: "))
+prime_set = []
+div = 2
+num = numb
+while num != 1:
+    if num % div == 0:
+        if check_prime(div, prime_set):
+            num //= div
+        else:
+            div += 1
+    else:
+        div += 1
+print(f"Число {numb} имеет следующие простые множители: {prime_set}")
---
+++ Seminar 4/homework/task4.py
@@ -0,0 +1,13 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена
+# и записать в файл многочлен степени k.
+# Пример: - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+from random import randint, choice
+
+degree = int(input("Введите степень: "))
+with open('task4.txt', 'w', encoding='utf-8') as f:
+    f.write(f'Список многочленов со степенью {degree}\n')
+    for _ in range(randint(1, 10)):
+        sample = [f'{randint(0, 100)}*(x**{degree}) + {randint(0, 100)}*x + {randint(0, 100)} = 0\n',
+                  f'x**{degree} + {randint(0, 100)} = 0\n',
+                  f'{randint(0, 100)}*(x**{degree}) = 0\n']
+        f.write(choice(sample))
