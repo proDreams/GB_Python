6252772a4020a64e1c521638bcbc84ee3f7efbd2
proDream <sushkoos@gmail.com>
2022-11-09 00:01:36 +0400

    update seminar 5 tasks

--- Seminar 5/homework/task1.py
+++ Seminar 5/homework/task1.py
@@ -1,17 +1,17 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
 input_data = ['абв', 'обеспАбвечивать', 'приносить', 'западный', 'испытАнибве', 'уезжать', 'абвреформа', 'придумать',
               'присутствовать', 'иабвюнабвь', 'подлинный']
-ref = set(i for i in input('Введите строку с символами: ').lower())
+ref = set(input('Введите строку с символами: ').lower())
 print(input_data, [data for data in input_data if not ref <= set(data.lower())], sep='\n')
-
+                                                    # ref.issubset(set(data.lower()))
 # Вариант когда слова построчно
 # with open('task1_input.txt', 'r', encoding='utf-8') as input_file:
 #     input_data = input_file.readlines()
 # with open('task1_output.txt', 'w', encoding='utf-8') as output_file:
 #     # for data in input_data:
-#     #     if not ref < set(data.lower()):
+#     #     if not ref <= set(data.lower()):
 #     #         output_file.write(f'{data}')
-#     output_file.writelines([data for data in input_data if not ref < set(data.lower())])
+#     output_file.writelines([data for data in input_data if not ref <= set(data.lower())])
 
 
 exit()
--- Seminar 5/homework/task4_zip.py
+++ Seminar 5/homework/task4_zip.py
@@ -2,12 +2,12 @@
 # Запаковщик
 import os.path
 
-
 with open('task4_zip_input.txt', 'r', encoding='utf-8') as input_file:
     input_data = input_file.readlines()[0]
 print(f'Исходная строка: {input_data}\n'
       f'Размер файла {os.path.getsize("task4_zip_input.txt")} байт')
 
+'vvvvvvvvvvsssssssssbbbbb'
 with open('task4_output.txt', 'w', encoding='utf-8') as output_file:
     result = ''
     count = 0
@@ -17,13 +17,9 @@ with open('task4_output.txt', 'w', encoding='utf-8') as output_file:
                 count += 1
             else:
                 break
-        if count > 9:
-            temp = count
-            while temp > 0:
-                result += f'{9 if temp > 9 else temp}{input_data[0]}'
-                temp -= 9
-        else:
-            result += f'{count}{input_data[0]}'
+        while count > 0:
+            result += f'{9 if count > 9 else count}{input_data[0]}'
+            count -= 9
         input_data = input_data[count:]
         count = 0
         if len(input_data) == 0:
