42f15b792fcbd67c157d4afdadb74fb083ab4ca2
proDream <sushkoos@gmail.com>
2022-11-11 13:59:39 +0400

    ADD seminar 6 tasks

---
+++ Seminar 6/homework/Other_variants/task2_others.py
@@ -0,0 +1,35 @@
+from random import randint
+
+lst = [randint(1, 10) for _ in range(10)]
+# lst = [1, 2, 3, 5, 1, 5, 3, 10]
+
+####################
+# Вариант первый.
+print(f'Сгенерированный список: {lst}\n'
+      f'Уникальные элементы: {list(filter(lambda x: lst.count(x) < 2, set(lst)))}\n'
+      f'Дубликаты: {list(filter(lambda x: lst.count(x) > 1, set(lst)))}\n'
+      f'Список без дубликатов: {list(set(lst))}')
+
+
+####################
+# Вариант второй.
+lst2, lst3 = [], []
+for i in set(lst):
+    if lst.count(i) > 1:
+        lst3.append(i)
+    else:
+        lst2.append(i)
+print(f'Сгенерированный список: {lst}\n'
+      f'Уникальные элементы: {lst2}\n'
+      f'Дубликаты: {lst3}\n'
+      f'Список без дубликатов: {list(set(lst))}')
+
+
+####################
+# Вариант третий.
+lst2 = [i for i in set(lst) if lst.count(i) < 2]
+lst3 = [i for i in set(lst) if lst.count(i) > 1]
+print(f'Сгенерированный список: {lst}\n'
+      f'Уникальные элементы: {lst2}\n'
+      f'Дубликаты: {lst3}\n'
+      f'Список без дубликатов: {list(set(lst))}')
---
+++ Seminar 6/homework/task1.py
@@ -0,0 +1,66 @@
+# Напишите программу вычисления арифметического выражения заданного строкой.
+# Используйте операции +,-,/,*. приоритет операций стандартный.
+# Пример:
+# 2+2 => 4;
+# 1+2*3 => 7;
+# 1-2*3 => -5;
+# Добавьте возможность использования скобок, меняющих приоритет операций.
+# Пример:
+# 1+2*3 => 7;
+# (1+2)*3 => 9;
+# Очевиднейший вариант.
+# print(eval(input('Введите выражение: ')))
+#################################################################################
+from decimal import Decimal
+
+
+def create_lists(text):
+    num = ''
+    op = ''
+    for i in text:
+        if i.isdigit() or i == '.':
+            num += i
+        elif i == ' ':
+            continue
+        else:
+            num += ' '
+            op += i
+    return list(map(Decimal, num.split())), list(op)
+
+
+def calc(num, op):
+    while len(num) > 1:
+        temp = 0
+        prod_div = min(op.index('*') if '*' in op else len(op),
+                       op.index('/') if '/' in op else len(op))
+        add_sub = min(op.index('-') if '-' in op else len(op),
+                      op.index('+') if '+' in op else len(op))
+        if prod_div != len(op):
+            if op[prod_div] == '*':
+                temp = num[prod_div] * num[prod_div + 1]
+            elif op[prod_div] == '/':
+                temp = num[prod_div] / num[prod_div + 1]
+            del num[prod_div]
+            num[prod_div] = temp
+            del op[prod_div]
+        elif add_sub != len(op):
+            if op[add_sub] == '+':
+                temp = num[add_sub] + num[add_sub + 1]
+            elif op[add_sub] == '-':
+                temp = num[add_sub] - num[add_sub + 1]
+            del num[add_sub]
+            num[add_sub] = temp
+            del op[add_sub]
+    return num
+
+
+input_text = '-5 * 6'
+while ')' in input_text:
+    right = input_text.find(')')
+    left = input_text[:right].rfind('(')
+    temp_n, temp_o = create_lists(input_text[left + 1:right])
+    temp_n = str(*calc(temp_n, temp_o))
+    input_text = input_text[:left] + temp_n + input_text[right + 1:]
+numbers, operators = create_lists(input_text)
+calc(numbers, operators)
+print(numbers[0].normalize())
